755d5f9ec9abb5a462b5b0be9389e4c463ceddce
Ludo <ludorouge7@gmail.com>
2024-09-17 09:36:00 +0200

    Commit test

---
+++ inscription.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="rstyle.css">
+</head>
+<body>  
+        <?php if ($error_message): ?>
+            <div class="alert alert-danger">
+                <?php echo ($error_message) ?>
+            </div>
+        <?php endif; ?>
+
+        <form method="POST" action="login.php">
+            <div>
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" id="email" required>
+            </div>
+            <div>
+                <label for="password" class="form-label">Mot de passe</label>
+                <input type="password" name="password" class="form-control" id="password" required>
+            </div>
+            <input type="submit" value="Login">
+        </form>
+</body>
+</html>
