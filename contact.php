c54ad0e7b8a413ddbb0059b0aae768fe6f79b26b
Ludo <ludorouge7@gmail.com>
2024-10-01 09:46:00 +0200

    optimisation code

--- contact.php
+++ contact.php
@@ -26,20 +26,6 @@
             margin: 0 15px;
             text-decoration: none;
         }
-        .video-container {
-            position: relative;
-            width: 100%;
-            height: calc(100vh - 50px); /* Adjust height if needed */
-            overflow: hidden;
-        }
-        .video-container iframe {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            border: 0;
-        }
     </style>
 </head>
 <body>
