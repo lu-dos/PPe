9230dafacf39e4314f2f61dc186570361a82b20a
Ludo <ludorouge7@gmail.com>
2024-10-15 09:33:40 +0200

    créa reserv

---
+++ reservation.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Reservation</title>
+    <link rel="stylesheet" type="text/css" href="rstyle.css">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
+        .navbar {
+            background-color: #333;
+            padding: 10px;
+            display: flex;
+            justify-content: space-between; /* Space between left and right content */
+            align-items: center;
+        }
+        .navbar .links {
+            display: flex;
+        }
+        .navbar a {
+            color: white;
+            margin: 0 15px;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>  
+<div class="navbar">
+        <div class="links">
+            <a href="acceuil.html">Accueil</a>
+            <a href="#">Réserver un Terrain</a>
+            <a href="contact.php">Contacter le Créateur</a>
+        </div>
+        <a href="login.php">Se Connecter</a>
+    </div>
+    
+    <ul>
+        <li><a href="acceuil.html">Retour</a></li>
+    </ul>
+
+</body>
+</html>
