0970897492e1e8a535bc49fa4fc07eeffc98fcc6
Ludo <ludorouge7@gmail.com>
2024-11-12 12:06:00 +0100

    login

--- inscription.php
+++ inscription.php
@@ -61,7 +61,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="rstyle.css">
     <title>Inscription</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 
--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <div class="navbar">
     <div class="links">
         <a href="accueil.php">Accueil</a>
-        <?php if (isset($_SESSION['IsClient']) && $_SESSION['IsClient'] == chr(1) || isset($_SESSION['IsAdmin']) && $_SESSION['IsAdmin'] == chr(1)): ?>
+        <?php if (isset($_SESSION['IsClient']) && $_SESSION['IsClient'] == "1" || isset($_SESSION['IsAdmin']) && $_SESSION['IsAdmin'] == "1"): ?>
             <a href="reservation.php">Réserver un Terrain</a>
             <a href="contact.php">Contacter le Créateur</a>
         <?php endif; ?>
@@ -15,54 +15,38 @@
         </form>
     <?php endif; ?>
 </div>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: Arial, sans-serif;
-        }
-        .navbar {
-            background-color: #333;
-            padding: 10px;
-            display: flex;
-            align-items: center;
 
-        }
-        .navbar .links {
-            display: flex;
-        }
-        .navbar a, .logout-button {
+<style>
+    body, html {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+    }
+    .navbar {
+        background-color: #333;
+        padding: 10px;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        box-sizing: border-box;
+    }
+    .navbar .links {
+        display: flex;
+    }
+    .navbar a, .logout-button {
         color: white;
         margin: 0 15px;
         text-decoration: none;
         font-size: 17px;
-        }   
-        .logout-button {
+    }   
+    .logout-button {
         background: none;
         border: none;
         cursor: pointer;
         color: white;
-        }
-/* CSS pour la barre de navigation */
-nav {
-    background-color: #333; /* Couleur de fond de la barre */
-    width: 100%; /* Étendre la barre sur toute la largeur */
-    display: flex;
-    justify-content: space-around; /* Espacement égal entre les éléments */
-    padding: 1em 0; /* Ajout de padding vertical */
-}
-
-nav a {
-    color: white;
-    text-decoration: none;
-    padding: 0.5em 1em;
-    font-size: 1.1em;
-}
-
-nav a:hover {
-    background-color: #555; /* Changement de couleur au survol */
-    border-radius: 5px;
-}
-
-
-    </style>
+    }
+    .navbar .links a:hover, .logout-button:hover {
+        background-color: #555;
+        border-radius: 5px;
+    }
+</style>
