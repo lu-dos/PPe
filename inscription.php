5152a04b3329846a7cbf2e877725a31835279f22
Ludo <ludorouge7@gmail.com>
2024-11-12 10:04:17 +0100

    changement requete

--- inscription.php
+++ inscription.php
@@ -15,6 +15,7 @@ try {
     $email = $_POST['email'];
     $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT); // Hash du mot de passe
     $ville = $_POST['ville'];
+    $IsClient = 1;
 
     // Vérification si l'email existe déjà
     $checkEmailQuery = "SELECT * FROM utilisateur WHERE mail = :email";
@@ -28,13 +29,14 @@ try {
         echo '<a href="inscription.html"><button>Retour à l\'inscription</button></a>';
     } else {
         // Insertion dans la base de données
-        $sql = "INSERT INTO utilisateur (nom, prenom, mail, mot_de_passe, ville, grade) VALUES (:nom, :prenom, :email, :mot_de_passe, :ville, 'client')";
+        $sql = "INSERT INTO utilisateur (nom, prenom, mail, mot_de_passe, ville, isClient) VALUES (:nom, :prenom, :email, :mot_de_passe, :ville, :,IsClient)";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':nom', $nom);
         $stmt->bindParam(':prenom', $prenom);
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':mot_de_passe', $mot_de_passe);
         $stmt->bindParam(':ville', $ville);
+        $stmt->bindParam(':IsClient', $IsClient);
 
         $stmt->execute();
 
